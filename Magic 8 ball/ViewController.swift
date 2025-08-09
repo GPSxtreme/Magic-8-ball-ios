07f9ca6a42a7692c7d7b5553c265e541b6ab07c5
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2025-08-09 20:30:27 +0530

    initial commit 🚀

--- Magic 8 ball/ViewController.swift
+++ Magic 8 ball/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var ball: UIImageView!
+    @IBOutlet weak var dialogLabel: UILabel!
+    let images: [UIImage] = [
+        UIImage(named: "ball1")!,
+        UIImage(named: "ball2")!,
+        UIImage(named: "ball3")!,
+        UIImage(named: "ball4")!,
+        UIImage(named: "ball5")!,
+    ];
+    let dialogs: [String] = [
+        "Balls balls, whole lotta balls",
+        "Whats the point of this app",
+        "This app sucks",
+        "Iam gay, you are gay",
+        "Xcode sucks, cant even vibe code on it 😔",
+        "This app's color palette sucks",
+        "You suck",
+        "Yo mama fat"
+    ];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onAsk(_ sender: Any) {
+        dialogLabel.text = dialogs.randomElement();
+        ball.image = images.randomElement();
+    }
+    
 }
 
