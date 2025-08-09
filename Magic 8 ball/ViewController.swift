5099dd76faac602dae0ca629b015e473b645a43c
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2025-08-09 19:16:43 +0530

    Initial Commit

---
+++ Magic 8 ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic 8 ball
+//
+//  Created by Prudhvi Suraaj on 09/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
